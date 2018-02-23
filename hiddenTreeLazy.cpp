ece5bc7fc96143863cb7b37172db0c1c9d836a41
EDIMAR JACOB BAUER <edimarjb@gmail.com>
2018-02-23 13:35:07 -0300

    Update hiddenTreeLazy.cpp

--- hiddenTreeLazy.cpp
+++ hiddenTreeLazy.cpp
@@ -1,170 +1,227 @@
 /*
-* Create by: Edimar Bauer
+* Create by: Edimar J. Bauer
 * Data: 19/12/2017
-* 
-* Consider no key equal -1
+*
+* Update: February 21, 2018
+*
+* Consider positive integer key
 */
 #include <iostream>
 #include <queue>
 
 using namespace std;
-#define SIZE (1 << 24)
+#define N (1 << 24)
+
 priority_queue<int, vector<int>, greater<int> > fila;
+int total;
 
-typedef struct node{
+
+struct Tree{
     int key;
-    struct node* left;
-    struct node* right;
-}Tree;
+    struct Tree *left, *right;
+};
 
-Tree* aloc(int x){
+Tree* newNode(int x);
+int findKey(Tree *tree, int &x, int low, int high);
 
-    Tree *node = (Tree*) malloc(sizeof(Tree));
-    if (node == NULL){
-        puts("Err of alocation");
+Tree* search(Tree *tree, int &x, int low, int high);
+void insert(Tree *&root, int &x, int low, int high);
+void remove(Tree *&tree, int &x, int low, int high);
+
+void print(Tree* tree, int low, int high, int i);
+void print(Tree* tree);
+void check_is_correct(Tree *tree, int low, int high);
+void check(Tree *tree, int low, int high);
+
+int main(){
+
+    Tree* tree = NULL;
+    int x, low = 0, high = N;
+
+    for (int j = 0; j < 2; j++){
+        puts("Inserting...");
+        for (int i = 0; i < N; i++){
+            x = rand() % N;
+            insert(tree, x, low, high);
+            //check_is_correct(tree, low, high);
+        }
+
+        puts("Removing...");
+        for (int i = 0; i < N; i++){
+            x = rand() % N;
+            remove(tree, x, low, high);
+            //check_is_correct(tree, low, high);
+        }
+    }
+    //print(tree);
+    //puts("");
+}
+
+//#####################################################################################################
+//#####################################################################################################
+
+Tree* newNode(int x){
+
+    Tree *tree = (Tree*) malloc(sizeof(Tree));
+    if (tree == NULL){
+        puts("Err of newNodeation");
         exit(0);
     }
 
-    node->key = x;
-    node->left = node->right = NULL;
-
-    return node;
+    tree->key = x;
+    tree->left = tree->right = NULL;
+    total++;
+    return tree;
 }
 
-int findingKey(Tree* node, int x, int low, int high){
+int findKey(Tree* tree, int &x, int low, int high){
 
     int middle;
     do{
         middle = (low + high) / 2;
 
         if (x <= middle){
-            node = node->left;
+            tree = tree->left;
             high = middle;
         }else{
-            node = node->right;
+            tree = tree->right;
             low = middle;
         }
 
-        if (node == NULL) return x;
-    }while(node->key != x);
+        if (tree == NULL) return x; //means the element was not found, so it will be inserted
+    }while(tree->key != x);
 
-    return -1;
+    return -1;  //means the element already exists in the tree, nothing to do
 }
 
-void insertion(Tree** tree, int x, int low, int high){
 
-    if (*tree == NULL){
-        *tree = aloc(x);
-        return;
+
+
+
+Tree* search(Tree* tree, int &x, int low, int high){
+
+    int middle;
+    while(tree != NULL){
+        if (x == tree->key) return tree;
+
+        middle = (low + high) / 2;
+        if (x <= middle){
+            tree = tree->left;
+            high = middle;
+        }else{
+            tree = tree->right;
+            low = middle;
+        }
     }
 
-    Tree *node = *tree;
+    return NULL;
+}
+
+void insert(Tree *&root, int &x, int low, int high){
+
+    if (root == NULL){
+        root = newNode(x);
+        return;
+    }
+    Tree *tree = root;
     int middle;
 
-    while(node->key != x){
-
-        if (node->key == -1){
-            node->key = findingKey(node, x, low, high);
+    while(tree->key != x){
+        if (tree->key == -1){
+            //look for the occurrence of x lower the tree
+            tree->key = findKey(tree, x, low, high);
             return;
         }
 
         middle = (low + high) / 2;
-
         if (x <= middle){
-            //base case
-            if (node->left == NULL){
-                node->left = aloc(x);
+            if (tree->left == NULL){
+                tree->left = newNode(x);
                 return;
             }
-            //down to the left
-            node = node->left;
+            tree = tree->left;
             high = middle;
         }else{
-            //base case
-            if (node->right == NULL){
-                node->right = aloc(x);
+            if (tree->right == NULL){
+                tree->right = newNode(x);
                 return;
             }
-            //down to the right
-            node = node->right;
+            tree = tree->right;
             low = middle;
         }
     }
 }
 
-Tree* searching(Tree* node, int x, int low, int high){
+void remove(Tree *&tree, int &x, int low, int high){
 
-    int middle;
-    while(node->key != x){
-        middle = (low + high) / 2;
+    if (tree == NULL) return;
 
-        if (x <= middle){
-            node = node->left;
-            high = middle;
-        }else{
-            node = node->right;
-            low = middle;
-        }
-
-        if (node == NULL) return NULL;
-    }
-
-    return node;
-}
-
-Tree* deletionLazy(Tree* node, int x, int low, int high){
-
-    if (node == NULL) return NULL;
-
-    if (node->key == x){
-        node->key = -1;
-    }else{
+    if (x == tree->key) tree->key = -1;
+    else{
         int middle = (low + high) / 2;
-        if (x <= middle){
-            node->left = deletionLazy(node->left, x, low, middle);
-        }else{
-            node->right = deletionLazy(node->right, x, middle, high);
-        }
+        if (x <= middle) remove(tree->left, x, low, middle);
+        else remove(tree->right, x, middle, high);
     }
 
-    if (node->left == NULL and node->right == NULL){
-        free(node);
-        return NULL;
+    if (tree->left == NULL and tree->right == NULL){
+        free(tree);
+        tree = NULL;
+        total--;
     }
-
-    return node;
 }
 
-void print(Tree* node){
 
-    if (node == NULL) return;
 
-    if (node->key != -1) fila.push(node->key);
-    print(node->left);
-    print(node->right);
-    if (node->key != -1){
+
+
+
+void print(Tree* tree, int low, int high, int i){
+
+    if (tree == NULL) return;
+
+    int middle = (low + high) / 2;
+    print(tree->left, low, middle, i+1);
+    printf("%d low: %d  high: %d  alt: %d\n", tree->key, low, high, i);
+    print(tree->right, middle, high, i+1);
+}
+
+void print(Tree* tree){
+
+    if (tree == NULL) return;
+
+    if (tree->key != -1) fila.push(tree->key);
+    print(tree->left);
+    print(tree->right);
+    if (tree->key != -1){
         cout << fila.top() << " ";
         fila.pop();
     }
 }
 
-int main(){
+void check_is_correct(Tree *tree, int low, int high){
 
-    Tree* tree = NULL;
-    int random, low, high;
-    low = 0;
-    high = SIZE;
-
-    for (int i = 0; i < SIZE; i++){
-        random = rand() % SIZE;
-        insertion(&tree, random, low, high);
+    int to = total;
+    check(tree, low, high);
+    if (total != 0) {
+        printf("Diference of %d elements\n", total);
+        exit(0);
     }
-
-    for (int i = 0; i < SIZE; i++){
-        random = rand() % SIZE;
-        tree = deletionLazy(tree, random, low, high);
-    }
-
-    print(tree);
-    puts("");
+    total = to;
 }
+
+void check(Tree *tree, int low, int high){
+
+    if (tree == NULL) return;
+
+    int middle = (low + high) / 2;
+    check(tree->left, low, middle);
+    total--;
+    if (tree->key != -1){
+        if (tree->key < low || tree->key > high){
+            printf("Node %d is between low: %d and high: %d\n", tree->key, low, high);
+            exit(0);
+        }
+    }
+    check(tree->right, middle, high);
+}
+
