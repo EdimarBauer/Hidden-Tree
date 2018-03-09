24cd56bdbccbed69eeb55eac00d3edb37572b246
EDIMAR JACOB BAUER <edimarjb@gmail.com>
2018-03-09 16:07:35 -0300

    Update heapHidden.cpp
    Added negative integers to heap and changed the pointer parent to a stack with the most left elements. Changed the method of insertion too.

--- heapHidden.cpp
+++ heapHidden.cpp
@@ -3,34 +3,33 @@
 * Email: edimarjb@gmail.com
 * Date: February 23, 2017
 *
+* Updated: March 09, 2018
+*
 * The variable 'last' and 'total' serve to test the algorithm
 */
 
 #include <bits/stdc++.h>
 
 using namespace std;
-#define N (1 << 25)
+#define N (1 << 24)
 
-int bound;
 int last, total;
 
 struct Tree{
     int key;
-    struct Tree *left, *right, *parent;
+    struct Tree *left, *right;
 };
 
+//pointer for the most left element (element of minimum)
 Tree *heap;
+stack<Tree*> pilha;   //store the parents of left nodes to update the heap after remotion
 
 Tree* newNode(int x);
-int nextPow(int x);
-Tree* search(Tree* tree, int &x);
+Tree* search(Tree *tree, int x);
 
-Tree* makeHead(Tree* node, int x);
-void createMiddleNode(Tree *tree, int x);
-void add(Tree* tree, int &x);
-void insert(Tree *&tree, int &x);
+void insert(Tree *&root, int x);
 
-int replace(Tree *&tree);
+int replace(Tree **tree);
 void popHeap(Tree *&tree);
 
 void print(Tree *tree, int i);
@@ -48,10 +47,11 @@ int main(){
         printf("Inserting...\n");
         for (int i = 0; i < N; i++){
             x = rand() % N;
+            if (rand() % 2) x = -x;
             insert(tree, x);
             //check_is_correct(tree);
         }
-
+        //print(tree, 0);
         printf("Removing...\n");
         while(heap != NULL){
             popHeap(tree);
@@ -71,7 +71,7 @@ int main(){
 
 
 
-Tree* newNode(Tree *parent, int x){
+Tree* newNode(int x){
 
     Tree *tree = (Tree*) malloc(sizeof(Tree));
     if (tree == NULL){
@@ -80,24 +80,11 @@ Tree* newNode(Tree *parent, int x){
     }
     tree->key = x;
     tree->left = tree->right = NULL;
-    tree->parent = parent;
     total++;
     return tree;
 }
 
-//search the superior limit of x that is pow of 2
-int nextPow(int x){
-
-    x |= (x>>1);
-    x |= (x>>2);
-    x |= (x>>4);
-    x |= (x>>8);
-    x |= (x>>16);
-
-    return x+1;
-}
-
-Tree* search(Tree* tree, int &x){
+Tree* search(Tree* tree, int x){
 
     while(tree != NULL){
         if (tree->key == x) return tree;
@@ -112,114 +99,80 @@ Tree* search(Tree* tree, int &x){
 
 
 
-Tree* makeHead(Tree* tree, int x){
-    bound = nextPow(x);
-    Tree* head = newNode(NULL, x);
-    head->left = tree;
-    tree->parent = head;
-    return head;
-}
+void insert(Tree *&root, int x){
 
-void createMiddleNode(Tree *tree, int x){
-    Tree *child = tree->left;
-    tree->left = newNode(tree, x);
-    tree->left->left = child;
-    child->parent = tree->left;
-}
+    if (root == NULL){
+        root = newNode(x);
+        heap = root;
+    }else{
+        Tree *tree = root;
+        int middle = 0;
+        int jump = (1 << 30);
+        bool right = 0;
 
-void add(Tree *tree, int &x){
+        while(tree->key != x){
+            if (x < middle){
+                if (x > tree->key) swap(x, tree->key);
 
-    int middle = bound >> 1;
-    int jump = middle >> 1;
-    int limit;
-    bool right = 0;
-
-    while(tree->key != x){
-        if (x < middle){
-            if (x > tree->key) swap(x, tree->key);
-
-            if (tree->left == NULL){
-                tree->left = newNode(tree, x);
-                if (right == 0) heap = tree->left;
-                break;
-            }
-            if (right == 0 && tree->left->key < jump){
-                //has a missing interval between the nodes, make node between then or jump to the next limit
-                limit = nextPow(tree->left->key);
-                if (x >= limit){
-                    createMiddleNode(tree, x);
+                if (tree->left == NULL){
+                    tree->left = newNode(x);
+                    if (right == 0){
+                        pilha.push(tree);
+                        heap = tree->left;
+                    }
                     break;
                 }
-                middle = limit >> 1;
-                jump = middle >> 1;
-            }else{
                 middle -= jump;
-                jump >>= 1;
-            }
-            tree = tree->left;
+                jump /= 2;
+                tree = tree->left;
+            }else if (x > middle){
+                if (x < tree->key) swap(x, tree->key);
 
-        }else if (x > middle){
-            if (x < tree->key) swap(x, tree->key);
-
-            if (tree->right == NULL){
-                tree->right = newNode(tree, x);
-                break;
-            }
-            tree = tree->right;
-            middle += jump;
-            jump >>= 1;
-            right = 1;
-
-        //else keeps x on the node
-        }else swap(x, tree->key);
+                if (tree->right == NULL){
+                    tree->right = newNode(x);
+                    break;
+                }
+                tree = tree->right;
+                middle += jump;
+                jump /= 2;
+                right = 1;
+            //else keeps x on the node
+            }else swap(x, tree->key);
+        }
     }
 }
 
-void insert(Tree *&tree, int &x){
-
-    if (tree == NULL){
-        bound = nextPow(x);
-        tree = newNode(NULL, x);
-        heap = tree;
-    }else if (bound <= x) tree = makeHead(tree, x);
-    else add(tree, x);
-}
 
 
 
 
+int replace(Tree **tree){
 
-int replace(Tree *&tree){
+    while((*tree)->left != NULL) tree = &((*tree)->left);
 
-    int x;
-    if (tree->left == NULL){
-        x = tree->key;
-        if (tree->right == NULL){
-            free(tree);
-            tree = NULL;
-        }else tree->key = replace(tree->right);
-    }else x = replace(tree->left);
+    int x = (*tree)->key;
+    if ((*tree)->right == NULL){
+        free(*tree);
+        *tree = NULL;
+    }else (*tree)->key = replace(&(*tree)->right);
 
     return x;
 }
 
 void popHeap(Tree *&tree){
 
-    if (heap == NULL) return;
+    if (tree == NULL) return;
     total--;
     if (heap->right == NULL){
-        if (heap->parent == NULL){
-            free(heap);
-            heap = NULL;
-            tree = NULL;
+        free(heap);
+        if (pilha.empty()){
+            heap = tree = NULL;
         }else{
-            heap = heap->parent;
-            free(heap->left);
+            heap = pilha.top();
+            pilha.pop();
             heap->left = NULL;
         }
-    }else{
-        heap->key = replace(heap->right);
-    }
+    }else heap->key = replace(&(heap->right));
 }
 
 
@@ -231,16 +184,13 @@ void print(Tree *tree, int i){
     if (tree == NULL) return;
 
     print(tree->left, i+1);
-    if (tree->parent)
-        cout << tree->key << " pai: " << tree->parent->key << " alt: " << i << endl;
-    else
-        cout << tree->key << " " << i << endl;
+    cout << tree->key << " " << i << endl;
     print(tree->right, i+1);
 }
 
 void check_is_correct(Tree *tree){
 
-    last = -1;
+    last = INT_MIN;
     int to = total;
     check(tree);
     if (total != 0){
